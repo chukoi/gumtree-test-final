9e6fa4748ecff980472832d84afb5943d83193db
Dean Heffernan <dean.heffernan@nextpracticehealth.com>
2025-03-23 17:29:50 +1100

    Upgraded to working condition again

--- webpack.config.js
+++ webpack.config.js
@@ -1,96 +1,94 @@
 // config
-const path = require('path');
-const dist = path.join(__dirname, 'dist');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const dist = path.join(__dirname, "dist");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 // webpack
 module.exports = [
   {
-    name: 'client',
-    target: 'web',
-    entry: ['./src/client.jsx', './sass/main.scss', 'react-hot-loader/patch'],
+    name: "client",
+    target: "web",
+    entry: ["./src/client.jsx", "./sass/main.scss", "react-hot-loader/patch"],
     output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'client.js',
-      publicPath: '/dist/'
+      path: path.join(__dirname, "dist"),
+      filename: "client.js",
+      publicPath: "/dist/",
     },
     resolve: {
-      extensions: ['.js', '.jsx']
+      extensions: [".js", ".jsx"],
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     module: {
       loaders: [],
       rules: [
         {
           test: /\.(png|jpg)$/,
-          loader: 'url-loader?limit=8192'
+          loader: "url-loader?limit=8192",
         },
         {
-          test: /\.jsx?$/,         // Match both .js and .jsx files
+          test: /\.jsx?$/, // Match both .js and .jsx files
           exclude: /node_modules/,
           loader: "babel-loader",
-          query:
-            {
-              presets: ['react']
-            }
+          query: {
+            presets: ["react"],
+          },
         },
         {
           test: /\.scss$/,
           use: [
             {
-              loader: 'style-loader'
+              loader: "style-loader",
             },
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               options: {
                 modules: false,
                 minimize: true,
                 importLoaders: 1,
-                localIdentName: '[hash:base64:10]',
-                sourceMap: true
-              }
+                localIdentName: "[hash:base64:10]",
+                sourceMap: true,
+              },
             },
             {
-              loader: 'sass-loader'
-            }
-          ]
-        }
-      ]
-    }
+              loader: "sass-loader",
+            },
+          ],
+        },
+      ],
+    },
   },
   {
-    name: 'server',
-    target: 'node',
-    entry: ['./src/server.jsx'],
+    name: "server",
+    target: "node",
+    entry: ["./src/server.jsx"],
     output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'server.js',
-      libraryTarget: 'commonjs2',
-      publicPath: '/dist/'
+      path: path.join(__dirname, "dist"),
+      filename: "server.js",
+      libraryTarget: "commonjs2",
+      publicPath: "/dist/",
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     resolve: {
-      extensions: ['.js', '.jsx']
+      extensions: [".js", ".jsx"],
     },
     module: {
       rules: [
         {
           test: /\.(png|jpg)$/,
-          loader: 'url-loader?limit=8192'
+          loader: "url-loader?limit=8192",
         },
         {
-          test: /\.jsx?$/,         // Match both .js and .jsx files
+          test: /\.jsx?$/, // Match both .js and .jsx files
           exclude: /node_modules/,
           loader: "babel-loader",
-          query:
-            {
-              presets: ['react']
-            }
-        }
-      ]
-    }
-  }
+          query: {
+            presets: ["react"],
+          },
+        },
+      ],
+    },
+  },
 ];
